e762c0dfc20805f4aa8da295429fbe470306800a
code-with-zulquar <codewithzulquar@gmail.com>
2024-09-19 21:55:16 +0530

    Fisrst Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Cards from "../Cards";
+import Form from "../Form";
+
+export default function App() {
+  return (
+    <div>
+      <Cards />
+      <Form />
+    </div>
+  );
+}
